49a4c803f8e3f82d7564a7ebec3ea51253e8174f
Johnson <seanjohnson@ebay.com>
2017-10-30 11:40:58 +1100

    Added javascript to the list of good things

--- js/main.js
+++ js/main.js
@@ -3,10 +3,11 @@ var d = new Date();
 const CURRENT_YEAR = d.getFullYear();
 const AWS_TIME = CURRENT_YEAR - 2006;
 const DOCKER_TIME = CURRENT_YEAR - 2013;
+const JS_TIME = CURRENT_YEAR - 2015;
 
 $(document).ready(function () {
-    console.log(`Ready! ${AWS_TIME} ${DOCKER_TIME}`);
     $('#CURRENT_YEAR').text(CURRENT_YEAR);
     $('#AWS_TIME').text(AWS_TIME);
     $('#DOCKER_TIME').text(DOCKER_TIME);
+    $('#JS_TIME').text(JS_TIME);
 });
